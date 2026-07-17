104abff73de063194de4a8b76f9e1f9e17367017
mr-sandip <animeleaker121@gmail.com>
2026-07-17 09:29:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,45 @@
 const mineflayer = require('mineflayer');
 
-const bot = mineflayer.createBot({
-  host: 'mr_sandip.aternos.me',
-  port: 62409,
-  username: 'SakhaAI',
-  version: '1.20.1'
-});
+function startBot() {
+  const bot = mineflayer.createBot({
+    host: 'mr_sandip.aternos.me',
+    port: 62409,
+    username: 'SakhaAI',
+    version: '1.20.1'
+  });
 
-bot.on('login', () => {
-  console.log('SakhaAI joined the server!');
-});
+  bot.on('login', () => {
+    console.log('SakhaAI joined the server!');
+  });
 
-bot.on('spawn', () => {
-  bot.chat('Hello! Mu SakhaAI 😊');
-});
+  bot.on('spawn', () => {
+    bot.chat('Hello! Mu SakhaAI 😊');
+  });
 
-bot.on('chat', (username, message) => {
-  if (username === bot.username) return;
+  bot.on('chat', (username, message) => {
+    if (username === bot.username) return;
 
-  if (message === 'hi') {
-    bot.chat('Hello ' + username + '!');
-  }
+    if (message === 'hi') {
+      bot.chat('Hello ' + username + '!');
+    }
 
-  if (message === 'come') {
-    bot.chat('Mu asuchi!');
-  }
-});
+    if (message === 'come') {
+      bot.chat('Mu asuchi!');
+    }
+  });
 
-bot.on('error', console.log);
-bot.on('end', () => console.log('Disconnected'));
+  bot.on('end', () => {
+    console.log('Disconnected! Rejoining in 5 seconds...');
+    setTimeout(startBot, 5000);
+  });
+
+  bot.on('kicked', (reason) => {
+    console.log('Kicked:', reason);
+  });
+
+  bot.on('error', (err) => {
+    console.log(err);
+  });
+}
+
+startBot();
